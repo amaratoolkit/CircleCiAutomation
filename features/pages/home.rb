a3a7209b86de75b65e4e505e37ddf253f71ac228
Shiv <siva9kolli@gmail.com>
2020-05-29 18:44:39 +0530

    page file updated

--- features/pages/home.rb
+++ features/pages/home.rb
@@ -1,9 +1,21 @@
 require 'selenium-webdriver'
+require 'rubygems'
 #require "chromedriver-helper"
 class HomePage
 
 def start_server
     @driver=Selenium::WebDriver.for :safari
+    # Input capabilities
+    # caps = Selenium::WebDriver::Remote::Capabilities.new
+    # caps['browser'] = 'Safari'
+    # caps['browser_version'] = '13.0'
+    # caps['os'] = 'OS X'
+    # caps['os_version'] = 'Catalina'
+    #
+    # @driver = Selenium::WebDriver.for(:remote,
+    #   :url => "http:127.0.0.1:4444/wd/hub",
+    #   :desired_capabilities => caps)
+
     @driver.manage.window.maximize
 end
 
