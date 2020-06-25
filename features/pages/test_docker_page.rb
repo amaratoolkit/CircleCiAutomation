5dec5c4ca2532533a9733f200d05b88181f52c92
Shiv <siva9kolli@gmail.com>
2020-06-25 11:11:06 +0530

    test execution on dockers

--- features/pages/test_docker_page.rb
+++ features/pages/test_docker_page.rb
@@ -4,8 +4,28 @@ require "webdrivers"
 
 class DockerPage
 
-  def loginAsUser(){
-    @driver=Selenium::WebDriver.for :safari
+  def loginAsUser()
+    # @driver=Selenium::WebDriver.for :safari
+    # @driver.manage.window.maximize
+    # @driver.navigate.to "http://automationpractice.com/index.php"
+    caps = Selenium::WebDriver::Remote::Capabilities.send("chrome")
+    @driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
     @driver.manage.window.maximize
     @driver.navigate.to "http://automationpractice.com/index.php"
-  }
+  end
+
+  def navigateToPages()
+    puts("navigateToPages")
+    sleep 5
+    @driver.find_element(:xpath, "//a[@title='Log in to your customer account']").click()
+    sleep 5
+    @driver.find_element(:xpath, "//*[@id='email']").send_keys "testing username"
+    @driver.find_element(:xpath, "//*[@id='passwd']").send_keys "testing passwprd"
+    @driver.find_element(:id, "SubmitLogin").click
+  end
+
+  def signOnWeb()
+    puts("SIgn")
+    @driver.quit
+  end
+end
