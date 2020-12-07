b524e57df48e237282b0f3c6d5240c5e86243cf8
Shiv <siva9kolli@gmail.com>
2020-12-07 17:06:32 +0530

    execution on browser stack from CircleCI

--- features/pages/mobilePage.rb
+++ features/pages/mobilePage.rb
@@ -1,49 +1,48 @@
-require "rubygems"
-require "appium_lib"
-
+require 'rubygems'
+require 'appium_lib'
 
 class MobileLogin
-def start_server
-  caps = {}
-  caps["name"] = "Ruby Appium Example"
-  caps["deviceName"] = "Galaxy S10"
-  caps["platformName"] = "Android"
-  caps["version"] = "10.0"
-  caps["app"] = "mobile_apps/naukri.apk"
+  def start_server
+    caps = {}
+    caps['name'] = 'Ruby Appium Example'
+    caps['deviceName'] = 'Samsung Galaxy S10e'
+    caps['platformName'] = 'Android'
+    caps['build'] = 'Android Build Sample'
+    caps['project'] = 'Naukri'
+    caps['name'] = 'Login Scenario'
+    caps['version'] = '9.0'
+    caps['app'] = 'bs://894a3e3631eb11539ad9be734bda317b3f072e53'
 
-  #caps["realDevice"] = true
+    # caps["realDevice"] = true
 
-  #@appium_driver = Selenium::WebDriver.for :remote
-  @appium_driver = Appium::Driver.new({
-    "caps" => caps,
-    "appium_lib" => {
-      :server_url => "http://127.0.0.1:4723/wd/hub",
-    },
-  }, true)
-  @appium_driver = @appium_driver.start_driver
+    # @appium_driver = Selenium::WebDriver.for :remote
+    @appium_driver = Appium::Driver.new({
+                                          'caps' => caps,
+                                          'appium_lib' => {
+                                            server_url: 'http://siva266:7Wfwz8qNKAn9X4XyNTXE@hub.browserstack.com:80/wd/hub'
+                                          }
+                                        }, true)
+    @appium_driver = @appium_driver.start_driver
+  end
 
+  def login
+    @wait = Selenium::WebDriver::Wait.new(timeout: 60)
+    @wait.until { @appium_driver.find_element(:id, 'b_login').displayed? }
 
-end
+    @appium_driver.find_element(:id, 'b_login').click
+    sleep 5
+    @appium_driver.find_element(:id, 'et_email').send_keys 'sivakolli'
+    @appium_driver.find_element(:id, 'et_password').send_keys '123456'
+  end
 
-def login
-  @wait = Selenium::WebDriver::Wait.new(:timeout => 60)
-  @wait.until { @appium_driver.find_element(:id, "b_login").displayed? }
+  # result = driver.find_element(:accessibility_id, "sum")
 
-  @appium_driver.find_element(:id, "b_login").click
-  sleep 5
-  @appium_driver.find_element(:id, "et_email").send_keys "sivakolli"
-  @appium_driver.find_element(:id, "et_password").send_keys "123456"
-end
-
-
-#result = driver.find_element(:accessibility_id, "sum")
-
-# if (!result.nil?) && (result.text.eql? "15")
-#   puts "Test Passed"
-# else
-#   puts "Test Failed"
-# end
-def kill_server
-  @appium_driver.quit
-end
+  # if (!result.nil?) && (result.text.eql? "15")
+  #   puts "Test Passed"
+  # else
+  #   puts "Test Failed"
+  # end
+  def kill_server
+    @appium_driver.quit
+  end
 end
--- features/pages/test_docker_page.rb
+++ features/pages/test_docker_page.rb
@@ -1,32 +1,31 @@
 require 'selenium-webdriver'
 require 'rubygems'
-require "webdrivers"
+require 'webdrivers'
 
 class DockerPage
-
-  def loginAsUser()
+  def loginAsUser
     # @driver=Selenium::WebDriver.for :safari
     # @driver.manage.window.maximize
     # @driver.navigate.to "http://automationpractice.com/index.php"
-    caps = Selenium::WebDriver::Remote::Capabilities.send("chrome")
-    #@driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
-    @driver = Selenium::WebDriver.for(:remote, url: "http://siva518:UQZfAXgr43CUqsZKxhF6@hub.browserstack.com:80/wd/hub", desired_capabilities: caps)
+    caps = Selenium::WebDriver::Remote::Capabilities.send('chrome')
+    # @driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
+    @driver = Selenium::WebDriver.for(:remote, url: 'http://siva266:7Wfwz8qNKAn9X4XyNTXE@hub.browserstack.com:80/wd/hub', desired_capabilities: caps)
     @driver.manage.window.maximize
-    @driver.navigate.to "http://automationpractice.com/index.php"
+    @driver.navigate.to 'http://automationpractice.com/index.php'
   end
 
-  def navigateToPages()
-    puts("navigateToPages")
+  def navigateToPages
+    puts('navigateToPages')
     sleep 3
-    @driver.find_element(:xpath, "//a[@title='Log in to your customer account']").click()
+    @driver.find_element(:xpath, "//a[@title='Log in to your customer account']").click
     sleep 5
-    @driver.find_element(:xpath, "//*[@id='email']").send_keys "testing username"
-    @driver.find_element(:xpath, "//*[@id='passwd']").send_keys "testing passwprd"
-    @driver.find_element(:id, "SubmitLogin").click
+    @driver.find_element(:xpath, "//*[@id='email']").send_keys 'testing username'
+    @driver.find_element(:xpath, "//*[@id='passwd']").send_keys 'testing passwprd'
+    @driver.find_element(:id, 'SubmitLogin').click
   end
 
-  def signOnWeb()
-    puts("SIgn")
+  def signOnWeb
+    puts('SIgn')
     @driver.quit
   end
 end
