6439986245c3d52d8881cb620ce53fc204f00762
Siva <>
2022-07-26 10:15:11 +0530

    build reporter added

--- features/support/hook.rb
+++ features/support/hook.rb
@@ -15,18 +15,18 @@ After do
   #  @driver.quit
 end
 
-at_exit do
-  ReportBuilder.configure do |config|
-   config.json_path = "./report/"
-   config.input_path = "./report/"
-   config.report_path = "./report/Automation_Full_Report"
-   config.report_types = [:html]
-   config.report_title = "Android Automation Report"
-   config.color = "cyan"
-   config.additional_info = {
-     Environment: "CircleCI",
-   }
-    end
-
-    ReportBuilder.build_report
-end
+# at_exit do
+#   ReportBuilder.configure do |config|
+#    config.json_path = "./report/"
+#    config.input_path = "./report/"
+#    config.report_path = "./report/Automation_Full_Report"
+#    config.report_types = [:html]
+#    config.report_title = "Android Automation Report"
+#    config.color = "cyan"
+#    config.additional_info = {
+#      Environment: "CircleCI",
+#    }
+#     end
+#
+#     ReportBuilder.build_report
+# end
